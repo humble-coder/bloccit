09eff4e46222eae9be89ef6a8b09a68235fa1831
Mark A Brown <mark.philosophe@gmail.com>
2013-12-12 22:23:00 -0500

    Fixed syntax error in config/initializers/setup_mail_production.rb

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -7,6 +7,6 @@ if Rails.env.production?
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRID_PASSWORD'],
     :domain           => 'heroku.com',
-    :enable_starttls_auto = true
+    :enable_starttls_auto => true
   }
 end
