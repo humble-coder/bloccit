d77bb393b3f27aab61f190e018a365e3adccb67f
Mark A Brown <mark.philosophe@gmail.com>
2013-12-24 13:15:40 -0500

    attempted to refactor user specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,37 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+    factory :user_with_posts do
+      ignore do
+        posts_count 1
+      end
+
+      after(:build) do |user, evaluator|
+        create_list(:post, evaluator.posts_count, user: user)
+      end
+    end
+
+    factory :user_with_comments do
+      ignore do
+        comments_count 1
+      end
+
+      after(:build) do |user, evaluator|
+        create_list(:comment, evaluator.comments_count, user: user)
+      end
+    end
+
+    trait :as_member do
+      role "member"
+    end
+
+    trait :as_moderator do
+      role "moderator"
+    end
+
+    trait :as_admin do
+      role "admin"
+    end
   end
 end
