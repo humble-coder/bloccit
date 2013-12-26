2c6d9919d2eb49634ec3fcbb5efd518c0b431f72
Mark A Brown <mark.philosophe@gmail.com>
2013-12-26 10:16:18 -0500

    refactored some controller tests

--- spec/controllers/post_spec.rb
+++ spec/controllers/post_spec.rb
@@ -1,55 +1,27 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe PostsController do
-#   describe "GET index" do
-#     it "gets the index page" do
-#       get :index
-#       expect(response.status).to eq(200)
-#     end
+describe PostsController do
+  describe "GET index" do
+    it "gets the index page" do
+      get :index
+      expect(response.status).to eq(200)
+    end
 
-#     it "retrieves most popular posts" do
-#       expect(Post).to receive(:popular)
-#     end
+    # it "retrieves most popular posts" do
+    #   get :index
+    #   expect(Post).to receive(:where).with("posts.created_at > ?", 7.days.ago)
+    # end
 
-#     it "renders the index template" do
-#       get :index
-#       expect(response).to render_template("index")
-#     end
-#   end
-# end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+      expect(response.body).to eq ""
+    end
 
-# describe Topics::PostsController do
-
-#   describe "GET new"
-
-#   describe "POST create"
-#   describe "Post management" do
-#     it "creates a post and redirects to the post's page" do
-#       get :new
-#       expect(response).to render_template(:new)
-
-#       post "/posts", :post => {title => "some post", body => "blah blah blah blah blah blah blah blah blah blah blah blah"}
-
-#       expect(response).to redirect_to(assigns(:post))
-#       follow_redirect!
-
-#       expect(response).to render_template(:show)
-#       expect(response.body).to include("Post was saved.")
-#     end
-#   end
-# end
-
-
-
-
-# describe Topics::PostsController do
-#   render_views
-
-#   describe "GET show" do
-#     it "says topic name" do
-#       get :show
-#       expect(response.body).to include(:topic)
-#     end
-#   end
-# end
-
+    it "renders the posts/index template" do
+      get :index
+      expect(response).to render_template("posts/index")
+      expect(response.body).to eq ""
+    end
+  end
+end
